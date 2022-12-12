149e2de9a6249ea480874f15b167752e39e4760a
welw <mrozekss@gmail.com>
2022-12-12 07:25:03 +0100

    Created template for vim for python files

---
+++ file.py
@@ -0,0 +1,17 @@
+from collections import *
+import sys
+itr = (line for line in sys.stdin.read().split())
+INP = lambda: next(itr)
+
+# Returns next integer from STDIN
+def ni(): return int(INP())
+def nf(): return float(INP())
+
+# Returns next line
+def nl(): return [_ for _ in INP().split()]
+
+x=int(ni())
+y=int(ni())
+
+print(x)
+print(y)
