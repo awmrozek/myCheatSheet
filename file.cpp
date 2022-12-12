2a45d502d6522bd5d23f8b9307b6f4fc176f9f83
welw <mrozekss@gmail.com>
2022-12-12 07:25:05 +0100

    Added template for vim for C++ projects and NixOS installations

---
+++ file.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <set>
+#define LL long long int
+#define PB push_back
+#define ST first
+#define ND second
+#define INF 9999999999999
+#define REP(i,a,b) for(int i=a; i<b; ++i)
+
+using namespace std;
+
+int solution () {
+}
+
+int main () {
+	solution();
+}
+
